e771e3952dfbb5dc0ec9d55641be64b6224b5020
truxxu <danielfetrujillo@gmail.com>
2022-05-11 17:54:49 -0500

    implements login and signup on auth screen

--- src/organisms/AuthForm.js
+++ src/organisms/AuthForm.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-const AuthForm = ({ loginReq, clearError, isLoading }) => {
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
+
   const { control, handleSubmit } = useForm({
     defaultValues: {
       email: "",
@@ -12,8 +17,51 @@ const AuthForm = ({ loginReq, clearError, isLoading }) => {
     },
   });
 
-  const onSubmit = () => {
+  const switchAuthMode = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const createUser = async (email, password) => {
+    const response = await fetch("/api/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || "Something went wrong!");
+    }
+
+    return data;
+  };
+
+  const onSubmit = async (data) => {
+    // event.preventDefault();
     console.log("success!");
+
+    if (isLogin) {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: data.email,
+        password: data.password,
+      });
+
+      if (!result.error) {
+        // set some auth state
+        router.replace("/profile");
+      }
+    } else {
+      try {
+        const result = await createUser(data.email, data.password);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
@@ -50,10 +98,13 @@ const AuthForm = ({ loginReq, clearError, isLoading }) => {
               </Form.Group>
             )}
           />
-          <Button type="submit">
-            <span>Action!</span>
+          <Button type="submit" className="mb-1">
+            <span>{isLogin ? "Login" : "Create Account"}</span>
           </Button>
         </Form>
+        <Button variant="secondary" type="button" onClick={switchAuthMode}>
+          {isLogin ? "Create new account" : "Login with existing account"}
+        </Button>
       </Card.Body>
     </Card>
   );
